2467c8b4acfb4a06f4f2a314e09f97c15bdc03e6
Jeremy Miller <jgmiller88@gmail.com>
2017-08-06 14:31:41 -0700

    initial commit

---
+++ lib/life_elixir.ex
@@ -0,0 +1,18 @@
+defmodule LifeElixir do
+  @moduledoc """
+  Documentation for LifeElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LifeElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/life_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule LifeElixirTest do
+  use ExUnit.Case
+  doctest LifeElixir
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
