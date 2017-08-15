4bae72261f802cba410bd1b8257148d3467bf284
Jeremy Miller <jgmiller88@gmail.com>
2017-08-15 08:38:27 -0700

    switch documentation of private functions

--- lib/life_elixir.ex
+++ lib/life_elixir.ex
@@ -29,13 +29,8 @@ defmodule LifeElixir do
   # Callbacks
   #############
 
-  @doc """
-  Get all living cells and asynchronously call `tick` on each one.
-  Wait for all cell ticks to finish.  Create new cells and destroy
-  dead cells.
-
-  Always returns `{:reply, :ok, []}`.
-  """
+  # Get all living cells and asynchronously call `tick` on each one.
+  # Wait for all cell ticks to finish.  Create new cells and destroy dead cells.
   def handle_call(:tick, _from, []) do
     get_cells()
     |> tick_cells
@@ -45,48 +40,30 @@ defmodule LifeElixir do
     {:reply, :ok, []}
   end
 
-  @doc """
-  Returns list of PIDs of all living cells.
-  """
+  # Returns list of PIDs of all living cells.
   defp get_cells, do: Cell.Supervisor.get_living_cells
 
-  @doc """
-  Spawn a Task to call `tick` on each cell.
-
-  Returns all spawned Tasks.
-  """
+  # Spawn a Task to call `tick` on each cell.
+  # Returns all spawned Tasks.
   defp tick_cells(cells) do
     map(cells, &(Task.async(fn -> Cell.tick(&1) end)))
   end
 
-  @doc """
-  Await all Tasks to finish their cell `ticks`.
-
-  Each cell `tick` retuns `{Cell positions to create, Cell positions to destroy}`
-  """
+  # Await all Tasks to finish their cell `ticks`.
+  # Each cell `tick` retuns `{[Cell positions to create], [Cell positions to destroy]}`
   defp wait_for_ticks(asyncs) do
     map(asyncs, &Task.await/1)
   end
 
-  @doc """
-  Consolidates all the returned cell positions into lists of positions to create and destroy.
-
-  Returns a tuple containing two lists:
-  1. Cells `to_create`.
-  2. Cells `to_destroy`.
-  """
+  # Consolidates all the returned cell positions into lists of positions to create and destroy.
+  # Returns `{[Cell positions to create], [Cell positions to destroy]}`.
   defp consolidate_cell_updates(ticks), do: reduce(ticks, {[], []}, &consolidate_ticks/2)
 
-  @doc """
-  Accumulates and de-duplicates the positions returned from all cell `ticks`.
-  """
   defp consolidate_ticks({create, destroy}, {acc_create, acc_destroy}) do
     {acc_create ++ create, acc_destroy ++ destroy}
   end
 
-  @doc """
-  Creates cells at `to_create` positions and destroys cells at `to_destroy` positions.
-  """
+  # Creates cells at `to_create` positions and destroys cells at `to_destroy` positions.
   defp update_cells({to_create, to_destroy}) do
     map(to_create, &Cell.create/1)
     map(to_destroy, &Cell.destroy/1)
