d43a89d2ea8dbafae7cfadbdaa0278644a736560
Jeremy <jgmiller88@gmail.com>
2017-08-20 16:27:54 -0700

    updates for inch ci

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule LifeElixir.Mixfile do
       {:credo, "~> 0.8", only: :dev, runtime: false},
       {:dialyxir, "~> 0.5", only: :dev, runtime: false},
       {:exfmt, "~> 0.4", only: :dev, runtime: false},
-      {:inch_ex, ">= 0.0.0", only: :docs},
+      {:inch_ex, "~> 0.5", only: :docs, runtime: false},
       {:mix_test_watch, "~> 0.4", only: :dev, runtime: false}
     ]
   end
