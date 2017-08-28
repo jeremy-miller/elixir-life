aa0d13a6d8db3a1a03e05c2bd4198e2775f9959a
Jeremy <jgmiller88@gmail.com>
2017-08-28 08:04:38 -0700

    only test happy path

--- test/cell_test.exs
+++ test/cell_test.exs
@@ -9,13 +9,6 @@ defmodule CellTest do
     assert Registry.lookup(Cell.Registry, position) == [{pid, nil}]
     Supervisor.terminate_child(Cell.Supervisor, pid)
   end
-
-  test "start_link/1 returns error if cell at `position` is already started" do
-    position = {1, 1}
-    {:ok, pid} = Cell.start_link(position)
-    assert Cell.start_link(position) == {:error, {:already_started, pid}}
-    Supervisor.terminate_child(Cell.Supervisor, pid)
-  end
   
   test "create/1 creates a new cell a `position` in the Supervisor" do
     position = {1, 2}
@@ -26,23 +19,10 @@ defmodule CellTest do
     Supervisor.terminate_child(Cell.Supervisor, pid)
   end
 
-  test "create/1 returns error if cell with `position` already started" do
-    position = {1, 3}
-    {:ok, pid} = Cell.create(position)
-    assert Cell.create(position) == {:error, {:already_started, pid}}
-    Supervisor.terminate_child(Cell.Supervisor, pid)
-  end
-
   test "destroy/1 terminates the cell with the given `position`" do
     position = {1, 4}
     {:ok, pid} = Cell.create(position)
     assert Cell.destroy(pid) == :ok
-  end
-
-  test "destroy/1 returns error if invalid `position` is given" do
-    # try terminating the PID running this test,
-    # which is not in the Cell.Supervisor
-    assert Cell.destroy(self()) == {:error, :not_found}
   end
 
   test "tick/1 returns no cells to create and its own pid to destroy" do
--- test/life_elixir_test.exs
+++ test/life_elixir_test.exs
@@ -2,12 +2,6 @@ defmodule LifeElixirTest do
   use ExUnit.Case, async: true
   doctest LifeElixir
 
-  test "start_link/0 starts LifeElixir GenServer" do
-    {:ok, pid} = GenServer.start(LifeElixir, [])
-    assert Process.alive?(pid)
-    GenServer.stop(pid)
-  end
-
   test "tick/0 successfully executes" do
     assert LifeElixir.tick == :ok
   end
