b6c1ff558a7bb67b372745d271ae19827e37a6cf
Jeremy <jgmiller88@gmail.com>
2017-08-27 22:03:37 -0700

    add life elixir tests

--- test/life_elixir_test.exs
+++ test/life_elixir_test.exs
@@ -2,7 +2,13 @@ defmodule LifeElixirTest do
   use ExUnit.Case, async: true
   doctest LifeElixir
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "start_link/0 starts LifeElixir GenServer" do
+    {:ok, pid} = GenServer.start(LifeElixir, [])
+    assert Process.alive?(pid)
+    GenServer.stop(pid)
+  end
+
+  test "tick/0 successfully executes" do
+    assert LifeElixir.tick == :ok
   end
 end
